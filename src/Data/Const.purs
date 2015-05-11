892e5c152c8d0c2ba394c4ae116a774226aa3466
Gary Burgess <gary.burgess@gmail.com>
2015-05-11 23:10:29 +0100

    Updates for 0.7

--- src/Data/Const.purs
+++ src/Data/Const.purs
@@ -2,10 +2,12 @@
 
 module Data.Const where
 
-import Data.Functor.Contravariant (Contravariant, (>$<))
-import Data.Foldable (Foldable, foldr, foldl, foldMap)
+import Data.Bifoldable (Bifoldable)
+import Data.Foldable (Foldable)
+import Data.Functor.Contravariant (Contravariant)
+import Data.Functor.Invariant (Invariant, imapF)
 import Data.Monoid (Monoid, mempty)
-import Data.Traversable (Traversable, traverse, sequence)
+import Data.Traversable (Traversable)
 
 -- | The `Const` type constructor, which wraps its first type argument
 -- | and ignores its second. That is, `Const a b` is isomorphic to `a`
@@ -21,8 +23,7 @@ getConst :: forall a b. Const a b ->  a
 getConst (Const x) = x
 
 instance eqConst :: (Eq a) => Eq (Const a b) where
-  (==) (Const x) (Const y) = x == y
-  (/=) c         c'        = not (c == c')
+  eq (Const x) (Const y) = x == y
 
 instance ordConst :: (Ord a) => Ord (Const a b) where
   compare (Const x) (Const y) = compare x y
@@ -35,28 +36,31 @@ instance showConst :: (Show a) => Show (Const a b) where
   show (Const x) = "Const (" ++ show x ++ ")"
 
 instance semigroupoidConst :: Semigroupoid Const where
-  (<<<) _ (Const x) = Const x
+  compose _ (Const x) = Const x
 
 instance semigroupConst :: (Semigroup a) => Semigroup (Const a b) where
-  (<>) (Const x) (Const y) = Const (x <> y)
+  append (Const x) (Const y) = Const (x <> y)
 
 instance monoidConst :: (Monoid a) => Monoid (Const a b) where
   mempty = Const mempty
 
 instance functorConst :: Functor (Const a) where
-  (<$>) _ (Const x) = Const x
+  map _ (Const x) = Const x
+
+instance invariantConst :: Invariant (Const a) where
+  imap = imapF
 
 instance applyConst :: (Semigroup a) => Apply (Const a) where
-  (<*>) (Const x) (Const y) = Const (x <> y)
+  apply (Const x) (Const y) = Const (x <> y)
 
 instance bindConst :: (Semigroup a) => Bind (Const a) where
-  (>>=) (Const x) _ = Const x
+  bind (Const x) _ = Const x
 
 instance applicativeConst :: (Monoid a) => Applicative (Const a) where
   pure _ = Const mempty
 
 instance contravariantConst :: Contravariant (Const a) where
-  (>$<) _ (Const x) = Const x
+  cmap _ (Const x) = Const x
 
 instance foldableConst :: Foldable (Const a) where
   foldr _ z _ = z
